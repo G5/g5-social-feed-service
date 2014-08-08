32c43b3d23c4a7efd6e951725e6fc22c9553c5ea
johnlucia <johnmlucia@gmail.com>
2014-08-07 17:12:23 -0700

    caching google plus

--- app/controllers/google_plus_feed_controller.rb
+++ app/controllers/google_plus_feed_controller.rb
@@ -1,4 +1,6 @@
 class GooglePlusFeedController < ApplicationController
+  caches_action :show, expires_in: 1.hour
+  
   def show
     GooglePlus.api_key=ENV['GOOGLE_PLUS_API_KEY']
     page = GooglePlus::Person.get(params[:google_plus_page_id])
