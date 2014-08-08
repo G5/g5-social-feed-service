dff759f3a1c4a08a471edebcd093c092957980d8
johnlucia <johnmlucia@gmail.com>
2014-08-08 10:23:05 -0700

    try fixing on heroku end instead

--- app/controllers/google_plus_feed_controller.rb
+++ app/controllers/google_plus_feed_controller.rb
@@ -3,7 +3,7 @@ class GooglePlusFeedController < ApplicationController
   
   def show
     GooglePlus.api_key=ENV['GOOGLE_PLUS_API_KEY']
-    page = GooglePlus::Person.get(params[:google_plus_page_id], user_ip: "216.228.180.219")
+    page = GooglePlus::Person.get(params[:google_plus_page_id])
     feed = page.try(:list_activities).try(:items) || []
     render json: feed.to_json
   end
