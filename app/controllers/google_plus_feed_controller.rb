be12ce48cafc3e069c2b09ad8bc0a8c184a8fb6e
johnlucia <johnmlucia@gmail.com>
2014-08-08 09:24:46 -0700

    try passing the quotaUser option

--- app/controllers/google_plus_feed_controller.rb
+++ app/controllers/google_plus_feed_controller.rb
@@ -1,9 +1,9 @@
 class GooglePlusFeedController < ApplicationController
-  caches_action :show, expires_in: 1.hour
+  # caches_action :show, expires_in: 1.hour
   
   def show
     GooglePlus.api_key=ENV['GOOGLE_PLUS_API_KEY']
-    page = GooglePlus::Person.get(params[:google_plus_page_id])
+    page = GooglePlus::Person.get(params[:google_plus_page_id], {"quotaUser" => "g5socialfeed"})
     feed = page.try(:list_activities).try(:items) || []
     render json: feed.to_json
   end
