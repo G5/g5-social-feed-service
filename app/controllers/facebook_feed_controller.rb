474922bfb70d93b8f9b3506ec195d00361fb414d
johnlucia <johnmlucia@gmail.com>
2014-08-08 08:42:26 -0700

    play some defense

--- app/controllers/facebook_feed_controller.rb
+++ app/controllers/facebook_feed_controller.rb
@@ -5,7 +5,6 @@ class FacebookFeedController < ApplicationController
   def show
     page_id = params[:facebook_page_id]
 
-    # Should we hardcode these?
     fields = ["id", "from", "message", "picture", "link", "type", "created_time", "updated_time"]
 
     facebook_feed_uri = URI.encode("https://graph.facebook.com/#{page_id}/posts?access_token=#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}&fields=#{fields.join(',')}")
--- app/controllers/google_plus_feed_controller.rb
+++ app/controllers/google_plus_feed_controller.rb
@@ -4,7 +4,7 @@ class GooglePlusFeedController < ApplicationController
   def show
     GooglePlus.api_key=ENV['GOOGLE_PLUS_API_KEY']
     page = GooglePlus::Person.get(params[:google_plus_page_id])
-    google_plus_feed = page.list_activities.items
-    render json: google_plus_feed.to_json
+    feed = page.try(:list_activities).try(:items) || []
+    render json: feed.to_json
   end
 end
