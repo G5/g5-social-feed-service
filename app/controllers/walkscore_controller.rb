b8d048ef451d3939577a1a1730d89c65fb3b4b6b
Dave Reuss <dave.reuss@getg5.com>
2016-01-04 15:47:45 -0800

    Fix - Refactor to eliminate g5-hub api request

--- app/controllers/walkscore_controller.rb
+++ app/controllers/walkscore_controller.rb
@@ -1,32 +1,22 @@
 require 'uri'
 class WalkscoreController < ApplicationController
   caches_action :show, expires_in: 12.hours
-  
-  def walkscore_hub_info(walkscore_client, walkscore_location)
-    #G5 Hub endpoint
-    json_endpoint = walkscore_location.blank? ? "#{walkscore_client}" : "#{walkscore_client}/locations/#{walkscore_location}"
 
-    #Need to Account for a Client Site Only utilizing this string, would have to update client on hub to include lat / long.
-    location_info = URI.encode("http://g5-hub.herokuapp.com/clients/#{json_endpoint}.json")
-    location_response = HTTParty.get(location_info)
-  end
-
-  def walkscore_uri_method(walkscore_client, walkscore_location)
-    walkscore_get_fields = walkscore_hub_info(walkscore_client, walkscore_location).parsed_response
+  def walkscore_uri_method
     fields = {
-      :address => walkscore_get_fields["location"]["street_address_1"],
-      :city => walkscore_get_fields["location"]["city"],
-      :state => walkscore_get_fields["location"]["state"],
-      :lat => walkscore_get_fields["location"]["latitude"],
-      :lon => walkscore_get_fields["location"]["longitude"]
+      :address => params[:address],
+      :city => params["city"],
+      :state => params["state"],
+      :lat => params["lat"],
+      :lon => params["lon"]
     }
-    walkscore_score_uri = URI.encode("http://api.walkscore.com/score?format=json&address=#{fields[:address]} #{fields[:city]} #{fields[:state]}&lat=#{fields[:lat]}&lon=#{fields[:lon]}&wsapikey=#{ENV['WALKSCORE_API_KEY']}")
+    walkscore_score_uri = URI.encode("http://api.walkscore.com/score?format=json&address=#{fields[:address]}#{fields[:city]}#{fields[:state]}&lat=#{fields[:lat]}&lon=#{fields[:lon]}&wsapikey=#{ENV['WALKSCORE_API_KEY']}")
     response = HTTParty.get(walkscore_score_uri)
   end
 
   def show
     walkscore_json = "[]"
-      walkscore_json = walkscore_uri_method(params[:walkscore_client], params[:walkscore_location]).parsed_response
+    walkscore_json = walkscore_uri_method.parsed_response
     render json: walkscore_json
   end
 end
--- spec/controllers/walkscore_controller_spec.rb
+++ spec/controllers/walkscore_controller_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 RSpec.describe WalkscoreController, :type => :controller do
   before do 
-    controller.stub(:params).and_return({:walkscore_client => "g5-c-1t2d31r8-berkshire-communities", :walkscore_location => "g5-cl-i2nmuhcd-thorncroft-farms"})
+    controller.stub(:params).and_return({:address=>"201 Wilshire Blvd Ste 102", :city=>"Santa Monica", :state=>"CA", :lat=>"34.0180307", :lon=>"-118.500053"})
   end
 
   let(:walkscore_client) {controller.params[:walkscore_client]}
   let(:walkscore_location) {controller.params[:walkscore_location]}
 
-  it "Calls the G5-Hub API given a Client & Location URN from Params and returns some JSON" do
-    #could mock out the response so it's not hitting a real service
-    response = controller.walkscore_hub_info(walkscore_client, walkscore_location)
-    response.header['Content-Type'].should include 'application/json'
-  end
-
   it "returns a json response from Walkscore API" do
     #could mock out the response so it's not hitting a real service
-    response = controller.walkscore_uri_method(walkscore_client, walkscore_location)
+    response = controller.walkscore_uri_method
     response.header['Content-Type'].should include 'application/json'
   end
 end
