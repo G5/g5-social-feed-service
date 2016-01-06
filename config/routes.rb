9a6af056f6058c5fe40fec2723af74d32fea197b
Dave Reuss <dave.reuss@getg5.com>
2016-01-05 16:34:08 -0800

    Fix - output

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'facebook-feed/:facebook_page_id' => 'facebook_feed#show'
 
-  get 'walkscore-params/:walkscore_client' => 'walkscore#show'
+  get 'walkscore-params/:walkscore_client.json' => 'walkscore#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
