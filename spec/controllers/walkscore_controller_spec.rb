60b5d14ee1b75f97a8770446a8654d8131fc2c97
jgbarr <jeffbarrr@gmail.com>
2018-05-11 15:17:26 -0700

    refactor walkscore spec

--- spec/controllers/walkscore_controller_spec.rb
+++ spec/controllers/walkscore_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
+#mock request to walkscore API
+RSpec.configure do |config|
+    config.before(:each) do
+      stub_request(:get, /api.walkscore.com/).
+        with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+        to_return(
+          :status => 200,
+          :headers => {"Content-Type"=> "application/json"})
+    end
+end
+
 RSpec.describe WalkscoreController, :type => :controller do
   before do
     controller.stub(:params).and_return({:address=>"201 Wilshire Blvd Ste 102", :city=>"Santa Monica", :state=>"CA", :lat=>"34.0180307", :lon=>"-118.500053"})
@@ -8,16 +19,4 @@ RSpec.describe WalkscoreController, :type => :controller do
   let(:walkscore_client) {controller.params[:walkscore_client]}
   let(:walkscore_location) {controller.params[:walkscore_location]}
 
-  it "returns a json response from Walkscore API" do
-  # mock request to walkscore API
-    RSpec.configure do |config|
-      config.before(:each) do
-        stub_request(:get, /api.walkscore.com/).
-          with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-          to_return(
-            :status => 200,
-            :headers => {"Content-Type"=> "application/json"})
-        end
-      end
-    end
 end
