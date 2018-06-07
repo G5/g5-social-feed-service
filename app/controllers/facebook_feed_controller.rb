d41327488eed105cba5d3c88559662c3e560dc8f
johnlucia <johnmlucia@gmail.com>
2018-06-07 10:33:36 -0700

    simpler

--- app/controllers/facebook_feed_controller.rb
+++ app/controllers/facebook_feed_controller.rb
@@ -2,17 +2,16 @@ require 'uri'
 class FacebookFeedController < ApplicationController
 
   # Specify cache path since we serve this action from two different routes
-  caches_action :show, cache_path: Proc.new { params[:facebook_page_id] }, expires_in: 12.hours
+  # caches_action :show, cache_path: Proc.new { params[:facebook_page_id] }, expires_in: 12.hours
 
   def show
     page_id = params[:facebook_page_id] || (not_found and return)
 
-    unless data = api_down_response
-      fields = ["id", "from", "message", "picture", "link", "type", "created_time", "updated_time"]
-      facebook_feed_uri = URI.encode("https://graph.facebook.com/v2.12/#{page_id}/posts?access_token=#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}&fields=#{fields.join(',')}")
-      response = HTTParty.get(facebook_feed_uri)
-      data = response.parsed_response
-    end
+    # fields = ["id", "from", "message", "picture", "link", "type", "created_time", "updated_time"]
+    # facebook_feed_uri = URI.encode("https://graph.facebook.com/v2.12/#{page_id}/posts?access_token=#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}&fields=#{fields.join(',')}")
+    # response = HTTParty.get(facebook_feed_uri)
+    # data = response.parsed_response
+    data = api_down_response
 
     render json: data
   end
@@ -20,7 +19,6 @@ class FacebookFeedController < ApplicationController
   private
 
   def api_down_response
-    return false unless ENV['FACEBOOK_DOWN']
     { data: [ { id: "1",
                 message: "The Facebook API is currently down. Please try again later",
                 from: { id: "", name: "" }
