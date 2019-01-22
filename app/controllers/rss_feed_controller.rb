2dff39d761bf838808291f1a5462f83e06bd0761
johnlucia <johnmlucia@gmail.com>
2019-01-22 13:33:53 -0700

    caching back on

--- app/controllers/rss_feed_controller.rb
+++ app/controllers/rss_feed_controller.rb
@@ -1,6 +1,6 @@
 require 'uri'
 class RssFeedController < ApplicationController
-  # caches_action :show, expires_in: 1.hour
+  caches_action :show, expires_in: 1.hour
 
   def show
     feed_url = params[:feed_url] || (not_found and return)
