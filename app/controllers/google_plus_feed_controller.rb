ea95e19b687dfa93c217b0c04ee009792ffe6b97
johnlucia <johnmlucia@gmail.com>
2019-01-30 14:06:34 -0700

    no mo google plus

--- app/controllers/google_plus_feed_controller.rb
+++ app/controllers/google_plus_feed_controller.rb
@@ -1,10 +1,5 @@
 class GooglePlusFeedController < ApplicationController
-  caches_action :show, expires_in: 2.hours
-
   def show
-    GooglePlus.api_key=ENV['GOOGLE_PLUS_API_KEY']
-    page = GooglePlus::Person.get(params[:google_plus_page_id])
-    feed = page.try(:list_activities).try(:items) || []
-    render json: feed.to_json
+    render json: []
   end
 end
