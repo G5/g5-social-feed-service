661d12a61a909f0eebe1909b92dcecd72d380fb8
Aaron Webre <aaron.webre@g5platform.com>
2020-01-27 14:19:09 -0800

    Passing cache store URL into redis options hash

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-redis = Redis.new
+options = {}
+options[:url] = URI.parse(ENV["CACHE_STORE_URL"]) if ENV["CACHE_STORE_URL"]
+redis = Redis.new(options)
 API_STATUS = Redis::Namespace.new(:api_status, redis: redis)
 FACEBOOK_CACHE = Redis::Namespace.new(:facebook, redis: redis)
